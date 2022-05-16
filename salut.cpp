4eeb4c43b54d9bfbef7e56a0d49aab2b7ca7705a
lucanchling <luc.anchling@cpe.fr>
2022-05-16 12:05:14 +0200

    Su etc.. pour la sphere

---
+++ salut.cpp
@@ -0,0 +1,14 @@
+// Sphère 
+Su = vec3(-r*sin(u)*cos(v), r*cos(u)*cos(v), 0);
+Sv = vec3(-r*cos(u)*sin(v), -r*sin(u)*sin(v), r*cos(v));
+Suu = vec3(-r*cos(u)*cos(v), -r*sin(u)*cos(v), 0);
+Svv = vec3(-r*cos(u)*cos(v), -r*sin(u)*cos(v), -r*sin(v));
+Suv = vec3(r*sin(u)*sin(v), -r*cos(u)*sin(v), 0);
+ 
+// Paraloïde
+
+// Caténoïde
+
+// Hélicoïde
+
+// Pseudo Sphère
